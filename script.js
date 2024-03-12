52f08eb9f4d95a86963bbdc228c565aa8263607c
kerm83 <kamarulkasmiran@gmail.com>
2024-03-12 16:57:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 // script.js
 
 document.getElementById("button1").addEventListener("click", function () {
-  window.location.href = "TableSolat/Iftitah.html";
+  window.location.href = "MaknaSolat/TableSolat/Iftitah.html";
 });
 
 document.getElementById("button2").addEventListener("click", function () {
-  window.location.href = "TableSolat/AlFatihah.html";
+  window.location.href = "MaknaSolat/TableSolat/AlFatihah.html";
 });
 
 document.getElementById("button3").addEventListener("click", function () {
-  window.location.href = "./TableSolat/Rukuk.html";
+  window.location.href = "MaknaSolat/TableSolat/Rukuk.html";
 });
 
 document.getElementById("button4").addEventListener("click", function () {
-  window.location.href = "../TableSolat/Iktidal.html";
+  window.location.href = "MaknaSolat/TableSolat/Iktidal.html";
 });
 
 document.getElementById("button5").addEventListener("click", function () {
-  window.location.href = "../TableSolat/Sujud.html";
+  window.location.href = "MaknaSolat/TableSolat/Sujud.html";
 });
 
 document.getElementById("button6").addEventListener("click", function () {
-  window.location.href = "../TableSolat/DudukAntara2Sujud.html";
+  window.location.href = "MaknaSolat/TableSolat/DudukAntara2Sujud.html";
 });
 
 document.getElementById("button7").addEventListener("click", function () {
-  window.location.href = "../TableSolat/TahiyyatAwal.html";
+  window.location.href = "MaknaSolat/TableSolat/TahiyyatAwal.html";
 });
 
 document.getElementById("button8").addEventListener("click", function () {
-  window.location.href = "../TableSolat/TahiyyatAkhir.html";
+  window.location.href = "MaknaSolat/TableSolat/TahiyyatAkhir.html";
 });
 
 document.getElementById("button9").addEventListener("click", function () {
-  window.location.href = "../TableSolat/Qunut.html";
+  window.location.href = "MaknaSolat/TableSolat/Qunut.html";
 });
 
 document.getElementById("button10").addEventListener("click", function () {
-  window.location.href = "../TableSolat/QunutNazilah.html";
+  window.location.href = "MaknaSolat/TableSolat/QunutNazilah.html";
 });
